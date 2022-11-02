b97fb475b71154b946dcb49a4c59d504c0300482
spiderforrest <spider@spood.org>
2022-11-02 16:50:39 -0700

    got elemets

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-/* Imports */
+// grab elements/generate vars
+const climateInput = document.getElementsById('climate-input');
+const architectureInput = document.getElementsById('architecture-input');
+const timeInput = document.getElementsById('time-input');
+const climateImg = document.getElementsById('climate-img');
+const architectureImg = document.getElementsById('architecture-img');
+const timeImg = document.getElementsById('time-img');
 
-/* Get DOM Elements */
+// event listeners for image changes
 
-/* State */
+// update image function
 
-/* Events */
-
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+// event listener for submit button
