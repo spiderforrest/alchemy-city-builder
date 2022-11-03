4e139cb665caaf99f76d5348c56d7105fdead7d2
Jessica Mosby <jessicamosby@mac.com>
2022-11-03 19:56:25 -0400

    created slogan list and submit button functionality

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const timeInput = document.getElementById('time-input');
 const climateImg = document.getElementById('climate-img');
 const architectureImg = document.getElementById('architecture-img');
 const timeImg = document.getElementById('time-img');
+const submitButton = document.getElementById('submit-button');
+const sloganDisplay = document.getElementById('slogan-display');
+const sloganArray = [];
+const inputBox = document.getElementById('input-box');
 
 // event listeners for image changes
 climateInput.addEventListener('change', (e) => {
@@ -30,6 +34,15 @@ timeInput.addEventListener('change', (e) => {
     }
 });
 
-// update image function
-
 // event listener for submit button
+submitButton.addEventListener('click', () => {
+    sloganDisplay.textContent = '';
+    const userInput = inputBox.value;
+    sloganArray.push(userInput);
+    for (let item of sloganArray) {
+        const newElement = document.createElement('p');
+        newElement.textContent = item;
+        sloganDisplay.append(newElement);
+    }
+    inputBox.value = '';
+});
