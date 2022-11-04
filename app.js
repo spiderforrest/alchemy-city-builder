ff2c2e596ea38e37e4433b033a234a4fdd291ccb
spiderforrest <spider@spood.org>
2022-11-03 18:02:18 -0700

    added comments, refactored to functions, etc

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const sloganDisplay = document.getElementById('slogan-display');
 const changesArchitecture = document.getElementById('changes-architecture');
 const changesClimate = document.getElementById('changes-climate');
 const changesTime = document.getElementById('changes-time');
+// define vars
 const sloganArray = [];
 let climateChanges = 0;
 let architectureChanges = 0;
@@ -24,8 +25,10 @@ climateInput.addEventListener('change', (e) => {
     if (document.querySelector('#climate-input option[value=placeholder]')) {
         document.querySelector('#climate-input option[value=placeholder]').remove();
     }
+    // increment change variable
     climateChanges++;
-    updateCounters();
+    // update display
+    displayStats();
 });
 architectureInput.addEventListener('change', (e) => {
     const value = e.target.value;
@@ -34,7 +37,7 @@ architectureInput.addEventListener('change', (e) => {
         document.querySelector('#architecture-input option[value=placeholder]').remove();
     }
     architectureChanges++;
-    updateCounters();
+    displayStats();
 });
 timeInput.addEventListener('change', (e) => {
     const value = e.target.value;
@@ -43,10 +46,11 @@ timeInput.addEventListener('change', (e) => {
         document.querySelector('#time-input option[value=placeholder]').remove();
     }
     timeChanges++;
-    updateCounters();
+    displayStats();
 });
 
-function updateCounters() {
+function displayStats() {
+    // check if the text should be plural
     if (architectureChanges === 1) {
         changesArchitecture.textContent = `You changed the architecture ${architectureChanges} time.`;
     } else {
@@ -58,6 +62,7 @@ function updateCounters() {
     } else if (climateChanges === 1) {
         changesClimate.textContent = `You changed the climate 1 time.`;
     } else {
+        // scold them for not recycling
         changesClimate.textContent = `You changed the climate ${climateChanges} times (you monster).`;
     }
     if (timeChanges === 1) {
@@ -66,17 +71,22 @@ function updateCounters() {
         changesTime.textContent = `You changed the time period ${timeChanges} times.`;
     }
 }
-updateCounters();
+// call the function once to populate on page load
+displayStats();
 
-// event listener for submit button
-submitButton.addEventListener('click', () => {
+function displaySlogans() {
     sloganDisplay.textContent = '';
-    const userInput = inputBox.value;
-    sloganArray.push(userInput);
     for (const item of sloganArray) {
         const newElement = document.createElement('p');
         newElement.textContent = item;
         sloganDisplay.append(newElement);
     }
     inputBox.value = '';
+}
+
+// event listener for submit button
+submitButton.addEventListener('click', () => {
+    const userInput = inputBox.value;
+    sloganArray.push(userInput);
+    displaySlogans();
 });
